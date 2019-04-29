e9758519a3658143382167d63ee7103dcfaf9d41
Simone <simone.masoni@gmail.com>
2019-04-29 11:55:33 +0200

    Code formatting

--- DevoxxClientCommon/src/main/java/com/devoxx/model/Badge.java
+++ DevoxxClientCommon/src/main/java/com/devoxx/model/Badge.java
@@ -35,175 +35,299 @@ import javafx.beans.property.StringProperty;
 
 public class Badge extends Searchable {
 
-    @SuppressWarnings("unused")
-    public Badge() {
-    }
+	@SuppressWarnings("unused")
+	public Badge() {
+	}
 
-//    public Badge(String qr) {
-//        if (qr != null && ! qr.isEmpty() && qr.split("::").length == 8) {
-//            String[] split = qr.split("::");
-//            badgeId.set(split[0]);
-//            firstName.set(split[1]);
-//            lastName.set(split[2]);
-//            role.set(split[3]);
-//            programmingLanguages.set(split[4].replaceAll("\\|", ", "));
-//            city.set(split[5]);
-//            country.set(split[6]);
-//            email.set(split[7]);
-//        }
-//    }
+	// public Badge(String qr) {
+	// if (qr != null && ! qr.isEmpty() && qr.split("::").length == 8) {
+	// String[] split = qr.split("::");
+	// badgeId.set(split[0]);
+	// firstName.set(split[1]);
+	// lastName.set(split[2]);
+	// role.set(split[3]);
+	// programmingLanguages.set(split[4].replaceAll("\\|", ", "));
+	// city.set(split[5]);
+	// country.set(split[6]);
+	// email.set(split[7]);
+	// }
+	// }
 
-//    private StringProperty badgeId = new SimpleStringProperty();
-//    public StringProperty badgeIdProperty() { return badgeId; }
-//    public String getBadgeId() { return badgeId.get(); }
-//    public void setBadgeId(String badgeId) { this.badgeId.set(badgeId); }
+	// private StringProperty badgeId = new SimpleStringProperty();
+	// public StringProperty badgeIdProperty() { return badgeId; }
+	// public String getBadgeId() { return badgeId.get(); }
+	// public void setBadgeId(String badgeId) { this.badgeId.set(badgeId); }
 
-    private StringProperty id = new SimpleStringProperty();
-    public String getId() { return id.get(); }
-    public void setId(String id) { this.id.set(id); }
-    
-    private StringProperty name = new SimpleStringProperty();
-    public StringProperty nameProperty() { return name; }
-    public String getName() { return name.get(); }
-    public void setName(String name) { this.name.set(name); }
+	private StringProperty id = new SimpleStringProperty();
 
-//    private StringProperty lastName = new SimpleStringProperty();
-//    public StringProperty lastNameProperty() { return lastName; }
-//    public String getLastName() { return lastName.get(); }
-//    public void setLastName(String lastName) { this.lastName.set(lastName); }
+	public String getId() {
+		return id.get();
+	}
 
-    private StringProperty jobTitle = new SimpleStringProperty();
-    public StringProperty jobTitleProperty() { return jobTitle; }
-    public String getJobTitle() { return jobTitle.get(); }
-    public void setJobTitle(String jobTitle) {this.jobTitle.set(jobTitle); }
+	public void setId(String id) {
+		this.id.set(id);
+	}
 
-    private StringProperty language = new SimpleStringProperty();
-    public StringProperty languageProperty() { return language; }
-    public String getLanguage() { return language.get(); }
-    public void setLanguage(String language) {this.language.set(language); }
+	private StringProperty name = new SimpleStringProperty();
 
-    private IntegerProperty age = new SimpleIntegerProperty();
-    public IntegerProperty ageProperty() { return age; }
-    public int getAge() { return age.get(); }
-    public void setAge(int age) {this.age.set(age); }
+	public StringProperty nameProperty() {
+		return name;
+	}
 
-    private StringProperty gender = new SimpleStringProperty();
-    public StringProperty genderProperty() { return gender; }
-    public String getGender() { return gender.get(); }
-    public void setGender(String gender) {this.gender.set(gender); }
+	public String getName() {
+		return name.get();
+	}
 
-    private StringProperty company = new SimpleStringProperty();
-    public StringProperty companyProperty() { return company; }
-    public String getCompany() { return company.get(); }
-    public void setCompany(String company) {this.company.set(company); }
+	public void setName(String name) {
+		this.name.set(name);
+	}
 
-    private StringProperty programmingLanguages = new SimpleStringProperty();
-    public StringProperty programmingLanguagesProperty() {return programmingLanguages;}
-    public String getProgrammingLanguages() { return programmingLanguages.get(); }
-    public void setProgrammingLanguages(String programmingLanguages) {this.programmingLanguages.set(programmingLanguages);}
-    
-    private StringProperty city = new SimpleStringProperty();
-    public StringProperty cityProperty() {return city;}
-    public String getCity() { return city.get(); }
-    public void setCity(String city) {this.city.set(city);}
-    
-    private StringProperty country = new SimpleStringProperty();
-    public StringProperty countryProperty() {return country;}
-    public String getCountry() { return country.get(); }
-    public void setCountry(String country) {this.country.set(country);}
-    
-    private StringProperty email = new SimpleStringProperty();
-    public StringProperty emailProperty() { return email; }
-    public String getEmail() { return email.get(); }
-    public void setEmail(String email) { this.email.set(email); }
+	// private StringProperty lastName = new SimpleStringProperty();
+	// public StringProperty lastNameProperty() { return lastName; }
+	// public String getLastName() { return lastName.get(); }
+	// public void setLastName(String lastName) { this.lastName.set(lastName); }
 
-    private StringProperty details = new SimpleStringProperty();
-    public StringProperty detailsProperty() { return details; }
-    public String getDetails() { return details.get(); }
-    public void setDetails(String details) { this.details.set(details); }
+	private StringProperty jobTitle = new SimpleStringProperty();
 
-    @Override
-    public boolean contains(String keyword) {
-        if (keyword == null || keyword.isEmpty()) {
-            return false;
-        } 
-        final String lowerKeyword = keyword.toLowerCase(Locale.ROOT);
+	public StringProperty jobTitleProperty() {
+		return jobTitle;
+	}
 
-        return containsKeyword(getName(), lowerKeyword) ||
-        	   containsKeyword(getEmail(), lowerKeyword) ||
-        	   containsKeyword(getCompany(), lowerKeyword) ||
-        	   containsKeyword(getCity(), lowerKeyword)   ||
-        	   containsKeyword(getCountry(), lowerKeyword)   ||
-        	   containsKeyword(getProgrammingLanguages(), lowerKeyword)   ||
-               containsKeyword(getJobTitle(), lowerKeyword)   ||
-               containsKeyword(getDetails(), lowerKeyword);
-    }
+	public String getJobTitle() {
+		return jobTitle.get();
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Badge badge = (Badge) o;
-        return Objects.equals(getEmail(), badge.getEmail());
-    }
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle.set(jobTitle);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getEmail());
-    }
+	private StringProperty language = new SimpleStringProperty();
 
-    protected String safeStr(String s) {
-        return s == null? "": s.trim();
-    }
-    
-    private String escapeString(String string) {
+	public StringProperty languageProperty() {
+		return language;
+	}
+
+	public String getLanguage() {
+		return language.get();
+	}
+
+	public void setLanguage(String language) {
+		this.language.set(language);
+	}
+
+	private IntegerProperty age = new SimpleIntegerProperty();
+
+	public IntegerProperty ageProperty() {
+		return age;
+	}
+
+	public int getAge() {
+		return age.get();
+	}
+
+	public void setAge(int age) {
+		this.age.set(age);
+	}
+
+	private StringProperty gender = new SimpleStringProperty();
+
+	public StringProperty genderProperty() {
+		return gender;
+	}
+
+	public String getGender() {
+		return gender.get();
+	}
+
+	public void setGender(String gender) {
+		this.gender.set(gender);
+	}
+
+	private StringProperty company = new SimpleStringProperty();
+
+	public StringProperty companyProperty() {
+		return company;
+	}
+
+	public String getCompany() {
+		return company.get();
+	}
+
+	public void setCompany(String company) {
+		this.company.set(company);
+	}
+
+	private StringProperty programmingLanguages = new SimpleStringProperty();
+
+	public StringProperty programmingLanguagesProperty() {
+		return programmingLanguages;
+	}
+
+	public String getProgrammingLanguages() {
+		return programmingLanguages.get();
+	}
+
+	public void setProgrammingLanguages(String programmingLanguages) {
+		this.programmingLanguages.set(programmingLanguages);
+	}
+
+	private StringProperty city = new SimpleStringProperty();
+
+	public StringProperty cityProperty() {
+		return city;
+	}
+
+	public String getCity() {
+		return city.get();
+	}
+
+	public void setCity(String city) {
+		this.city.set(city);
+	}
+
+	private StringProperty country = new SimpleStringProperty();
+
+	public StringProperty countryProperty() {
+		return country;
+	}
+
+	public String getCountry() {
+		return country.get();
+	}
+
+	public void setCountry(String country) {
+		this.country.set(country);
+	}
+
+	private StringProperty email = new SimpleStringProperty();
+
+	public StringProperty emailProperty() {
+		return email;
+	}
+
+	public String getEmail() {
+		return email.get();
+	}
+
+	public void setEmail(String email) {
+		this.email.set(email);
+	}
+
+	private StringProperty details = new SimpleStringProperty();
+
+	public StringProperty detailsProperty() {
+		return details;
+	}
+
+	public String getDetails() {
+		return details.get();
+	}
+
+	public void setDetails(String details) {
+		this.details.set(details);
+	}
+
+	@Override
+	public boolean contains(String keyword) {
+		if (keyword == null || keyword.isEmpty()) {
+			return false;
+		}
+		final String lowerKeyword = keyword.toLowerCase(Locale.ROOT);
+
+		return containsKeyword(getName(), lowerKeyword) || containsKeyword(getEmail(), lowerKeyword)
+				|| containsKeyword(getCompany(), lowerKeyword) || containsKeyword(getCity(), lowerKeyword)
+				|| containsKeyword(getCountry(), lowerKeyword)
+				|| containsKeyword(getProgrammingLanguages(), lowerKeyword)
+				|| containsKeyword(getJobTitle(), lowerKeyword) || containsKeyword(getDetails(), lowerKeyword);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Badge badge = (Badge) o;
+		return Objects.equals(getEmail(), badge.getEmail());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getEmail());
+	}
+
+	protected String safeStr(String s) {
+		return s == null ? "" : s.trim();
+	}
+
+	private String escapeString(String string) {
 		return "\"" + string + "\"";
-	}    
-    
-    public String toCSV() {
-        StringBuilder csv = new StringBuilder();
-        csv.append(escapeString(safeStr(getName())));
-        csv.append(",").append(escapeString(safeStr(getEmail())));
-        csv.append(",").append(escapeString(safeStr(getLanguage())));
-        csv.append(",").append(safeStr(getAge()+""));
-        csv.append(",").append(escapeString(safeStr(getGender())));
-        csv.append(",").append(escapeString(safeStr(getCompany())));
-        csv.append(",").append(escapeString(safeStr(getCity())));        
-        csv.append(",").append(escapeString(safeStr(getCountry())));        
-        csv.append(",").append(escapeString(safeStr(getProgrammingLanguages()!=null?getProgrammingLanguages().replaceAll(", ", "\\|"):"")));
-        csv.append(",").append(escapeString(safeStr(getJobTitle())));
-        csv.append(",").append(escapeString(safeStr(getDetails())));
-        return csv.toString();
-    }
-    
+	}
+
+	public String toCSV() {
+		StringBuilder csv = new StringBuilder();
+		csv.append(escapeString(safeStr(getName())));
+		csv.append(",").append(escapeString(safeStr(getEmail())));
+		csv.append(",").append(escapeString(safeStr(getLanguage())));
+		csv.append(",").append(safeStr(getAge() + ""));
+		csv.append(",").append(escapeString(safeStr(getGender())));
+		csv.append(",").append(escapeString(safeStr(getCompany())));
+		csv.append(",").append(escapeString(safeStr(getCity())));
+		csv.append(",").append(escapeString(safeStr(getCountry())));
+		csv.append(",").append(escapeString(
+				safeStr(getProgrammingLanguages() != null ? getProgrammingLanguages().replaceAll(", ", "\\|") : "")));
+		csv.append(",").append(escapeString(safeStr(getJobTitle())));
+		csv.append(",").append(escapeString(safeStr(getDetails())));
+		return csv.toString();
+	}
 
 	public static String getCSVHeader() {
-    	return "\"name\",\"email\",\"language\",\"age\",\"gender\",\"company\",\"city\",\"country\",\"proglang\",\"jobtitle\"";
-    }
+		return "\"name\",\"email\",\"language\",\"age\",\"gender\",\"company\",\"city\",\"country\",\"proglang\",\"jobtitle\"";
+	}
 
-    public static Badge parseBadge(String qrCode) {
-    	Badge badge = new Badge();
-    	if (qrCode != null && !qrCode.trim().isEmpty()) {
-    		String[] components = qrCode.split(";");
-    		for (String component : components) {
+	public static Badge parseBadge(String qrCode) {
+		Badge badge = new Badge();
+		if (qrCode != null && !qrCode.trim().isEmpty()) {
+			String[] components = qrCode.split(";");
+			for (String component : components) {
 				String[] keyValue = component.split(":");
 				String key = keyValue[0].trim();
-				String value = keyValue.length==2?keyValue[1].trim():"";
+				String value = keyValue.length == 2 ? keyValue[1].trim() : "";
 				switch (key.toLowerCase()) {
-					case "name": badge.setName(value); break;
-					case "email": badge.setEmail(value); break;
-					case "language": badge.setLanguage(value); break;
-					case "age": badge.setAge(value.isEmpty()?0:Integer.parseInt(value)); break;
-					case "gender": badge.setGender(value); break;
-					case "company": badge.setCompany(value); break;
-					case "city" : badge.setCity(value); break;
-					case "country": badge.setCountry(value); break;
-					case "proglang": badge.setProgrammingLanguages(value); break;
-					case "jobtitle": badge.setJobTitle(value); break;						
+				case "name":
+					badge.setName(value);
+					break;
+				case "email":
+					badge.setEmail(value);
+					break;
+				case "language":
+					badge.setLanguage(value);
+					break;
+				case "age":
+					badge.setAge(value.isEmpty() ? 0 : Integer.parseInt(value));
+					break;
+				case "gender":
+					badge.setGender(value);
+					break;
+				case "company":
+					badge.setCompany(value);
+					break;
+				case "city":
+					badge.setCity(value);
+					break;
+				case "country":
+					badge.setCountry(value);
+					break;
+				case "proglang":
+					badge.setProgrammingLanguages(value);
+					break;
+				case "jobtitle":
+					badge.setJobTitle(value);
+					break;
 				}
 			}
-    	}
-    	return badge;
-    }
-    
+		}
+		return badge;
+	}
+
 }
